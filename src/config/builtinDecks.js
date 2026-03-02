836f9283b48fe01ec2949c9bda9a8780c2e02ce8
Steve Glick <steve.glick.24@gmail.com>
2026-03-02 15:30:25 -0500

    fix formatting

---
+++ src/config/builtinDecks.js
@@ -0,0 +1,16 @@
+// src/config/builtinDecks.js
+
+/**
+ * Built-in hard-coded decks.
+ * These will ALWAYS be ensured on startup (they reappear after refresh, even if deleted).
+ *
+ * Add your permanent Google Sheet tab links here.
+ */
+export const BUILTIN_DECK_SOURCES = [
+  // Example:
+  // {
+  //   id: "builtin_python",
+  //   name: "Python (Built-in)",
+  //   tabUrl: "https://docs.google.com/spreadsheets/d/<ID>/edit#gid=0",
+  // },
+];
